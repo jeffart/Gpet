34e78e05bb5a0b09004656f695b69ae4760158b6
jeffart <dekogha@gmail.com>
2014-02-17 17:09:27 +0100

    Modif 13
    Creation de la fonction d'activation d'un compte utulisation.
    a cette etape quand on click sur le lien du mail , le compte est activé.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -5,7 +5,7 @@ class UsersController extends AppController{
 
     public function beforeFilter(){
         parent::beforeFilter();
-        $this->Auth->allow('signup','login');
+        $this->Auth->allow('signup','login','activate');
     }
 
 
@@ -53,9 +53,33 @@ class UsersController extends AppController{
         }
     }
 
-    /**
-     * Permet de regénérer un mot de passe pour un utilisateur
-     */
+    // fonction d'activation d'un compte utulisation
+
+    public function activate($user_id, $token){  // cette fonction prend  2 parametres
+
+        // on recherche le premier utilisateur dont id et le token corespondent aux valeur passe en parametres
+        $user = $this->User->find('first', array(
+            'fields'     => array('id'),
+            'conditions' => array('id' => $user_id, 'token' => $token)
+        ));
+
+        // si on ne trouve pas d'utilistaeur c est que le lien de validation n'est pas bon
+        if (empty($user)) {
+            $this->Session->setFlash('Ce lien de validation ne semble pas bon');
+            return $this->redirect('/');  // on est rediriger vers la homepage
+        }
+        // si l'utilisateur est est trouvé
+        $this->Session->setFlash('Votre compte a bien été validé');
+        $this->User->save(array(
+            'id'     => $user['User']['id'], // on precise quel utilisateur on veut modifier.
+            'active' => 1,       // on met le champs active du user a 1
+            'token'  => ''       // et on vide le token
+        ));
+        // puis on est redirger vers la page de connexion. pas besoin de préciser le controller
+        // car on est deja dans le controller User.
+        return $this->redirect(array('action' => 'login'));
+    }
+
 
 
 
