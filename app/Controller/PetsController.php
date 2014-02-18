6cdb90288a0178c1f4a6b088e1864b3107733e0c
jeffart <dekogha@gmail.com>
2014-02-18 13:20:29 +0100

    Modif 31
    Creation de la fonction de suppression des animaux de l'utilisateur.

--- app/Controller/PetsController.php
+++ app/Controller/PetsController.php
@@ -30,8 +30,8 @@ class PetsController extends AppController{
         // si Des donnée ont été postées
         if (!empty($this->request->data)) {
             $this->request->data['Pet']['id'] = null; // on empeche a l'utilisateur de modifier ce champs
-            if(isset($pet)){
-                $this->request->data['Pet']['id'] = $pet['Pet']['id'];
+            if(isset($pet)){  // est ce que j'ai un animal
+                $this->request->data['Pet']['id'] = $pet['Pet']['id']; // si oui
             }
             $this->request->data['Pet']['user_id'] = $this->Auth->user('id'); // on definit le user_id de la table pet qui est ici l'id de lutilisateur
             if($this->Pet->save($this->request->data)){ // si les donnees sont bien sauvegardé
@@ -48,6 +48,26 @@ class PetsController extends AppController{
 
         $species = $this->Pet->Species->find('list');
         $this->set(compact('species')); // on envoi  egalement la liste des especes a la vue
+    }
+
+
+    public function delete($id){   // on lui passe l'id de l'animal en parametre
+        if(!$this->request->is('post')){  // une astuce qui permet de se proteger contre les faille csrf.
+            throw new NotFoundException();
+        }
+
+        // conditions le user_id de l'animal est le meme que celui de l'utilisateur connecter et l'id de l'animal est egale à l'id
+        // pour eviter de supprimer l'animal de quelqu'un d'autre.
+        $pet = $this->Pet->find('first', array(
+            'conditions' => array('Pet.user_id' => $this->Auth->user("id"), 'Pet.id' => $id)
+        ));
+        if(empty($pet)){ // si on n'a pas d'animal
+            $this->Session->setFlash("Vous ne pouvez pas éditer cet animal","flash"); // on envoi un message
+            return $this->redirect(array('action' => 'my')); // on est rediriger vers l'action my
+        }
+        $this->Session->setFlash("L'animal a bien été supprimé","flash"); // si tout ce passe bien
+        $this->Pet->delete($pet['Pet']['id']); // on supprime l'animal
+        return $this->redirect(array('action' => 'my')); // on est rediriger vers l'action my
     }
 
 
