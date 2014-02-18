33cccbc1bba03dab93debf33ce201f8d48a9de9a
jeffart <dekogha@gmail.com>
2014-02-18 12:19:52 +0100

    Modif 29
    Creation du model Species.php
    
    A ce niveau du travail on affiche maintenant la vue d'edition et ou
    d'ajout des animaux et les espece seront trié par ordre alphabétique
    dans la liste box de cette vue

---
+++ app/Model/Species.php
@@ -0,0 +1,6 @@
+<?php
+class Species extends AppModel{
+
+    public $order = 'Species.name ASC'; // on defini un systeme d'organistaion par defaut des espece( les espece seront trié par ordre alphabétique).
+
+}
