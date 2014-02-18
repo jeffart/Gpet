6084b4d1e636c7bf0dde6604b94469b20d6f1d34
jeffart <dekogha@gmail.com>
2014-02-18 11:50:52 +0100

    Modif 27
    On a rapidement rajouté une fonction de suppression des espece dans le
    controller SpeciesController.php

--- app/Controller/SpeciesController.php
+++ app/Controller/SpeciesController.php
@@ -6,7 +6,7 @@ class SpeciesController extends AppController{
         $species = $this->Species->find('all'); // on recherche toutes les especes
         $this->set(compact('species'));  // on envoi le resultat a la vue  admin_index
     }
-
+  // Fonction d'ajout et de modification des especes
     public function admin_edit($id = null){
         if (!empty($this->request->data)) { // si des données sont posté
             $this->Species->save($this->request->data); // on sauvegarde des données
@@ -15,6 +15,15 @@ class SpeciesController extends AppController{
         }else if($id){
             $this->request->data = $this->Species->findById($id); // cette ligne nous permet d'afficher le nom de lespece sur le formulaire d'edition des espece (admin_edit.ctp)
         }
+    }
+
+
+    // Fonction de supression des especes
+
+    public function admin_delete($id){  // elle prend en parametre l'id de l'espece
+        $this->Species->delete($id);
+        $this->Session->setFlash("L'espèce a bien été supprimée", "flash");
+        return $this->redirect(array('action' => 'index')); // on est rediriger vers la page admin index du controller species.
     }
 
 
